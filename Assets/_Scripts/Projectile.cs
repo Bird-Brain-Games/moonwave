324b809ce78650b388ed7a1782568480d7d0d24a
Goombario <graham.watson@uoit.net>
2018-01-11 08:42:29 -0500

    Initial Push

---
+++ Assets/_Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Projectile : MonoBehaviour {
+
+	public Rigidbody m_Rigidbody;
+	public PlayerStats m_PlayerStats;
+
+	// Collision Layers [Graham]
+	protected int m_PlanetLayer = 8;
+	protected int m_PlayerLayer = 9;
+    protected int m_SunsLayer = 12;
+	protected int m_ProjectileLayer = 10;
+
+	// member variables [Graham]
+	protected Vector2 m_Direction;
+	protected float m_Force;
+
+	// Used to set the values of the projectile [Graham]
+	public void Init(Vector2 a_direction, float a_force, PlayerStats a_PlayerStats)
+	{
+		m_Direction = a_direction.normalized;
+		m_Force = a_force;
+		m_PlayerStats = a_PlayerStats;
+	}
+}
---
+++ Assets/_Scripts/state machine/PlayerBoostChargeState.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBoostChargeState : State
+{
+
+    Vector3 m_Direction;
+
+    PlayerStats m_PlayerStats;
+    Controls m_Controls;
+    PlayerBoost m_Boost;
+    Animator m_Animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_PlayerStats = GetComponent<PlayerStats>();
+        m_Controls = GetComponent<Controls>();
+        m_Boost = GetComponent<PlayerBoost>();
+        m_Animator = GetComponentInChildren<Animator>();
+    }
+
+    public override void StateEnter()
+    {
+        m_Boost.EntryBoost();
+        m_Animator.SetTrigger("Boost Started");
+    }
+
+    override public void StateUpdate()
+    {
+        //set the players direction based off of the analog stick
+        m_Direction = new Vector3(m_Controls.GetMove().x, m_Controls.GetMove().y, 0.0f);
+
+        // Charge the boost [Graham] (pull the lever graham, The other lever)
+        m_Boost.ChargeBoost();
+
+        if (m_Controls.GetBoost(BUTTON_DETECTION.GET_BUTTON_UP))
+        {
+            // If the boost is released, fire the boost [Graham]
+            m_Boost.FireBoost();
+            ChangeState(m_PlayerStats.PlayerBoostActiveString);
+        }
+    }
+}
