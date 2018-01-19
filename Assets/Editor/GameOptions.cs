b4f537a70ded22c6ad6a40ea23a24268f73bc227
Campbell Hamilton <35320547+campbellHamilton@users.noreply.github.com>
2018-01-18 21:33:49 -0500

    pushed for testing backup.

---
+++ Assets/Editor/GameOptions.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class GameOptions : EditorWindow
+{
+    //The toggle state
+    bool wrapping;
+    //The GameObject we are changing in this case it is a prefab
+    GameObject wrappingPrefab;
+
+    private void Awake()
+    {
+        //Setup
+        wrapping = false;
+        wrappingPrefab = AssetDatabase.LoadAssetAtPath("Assets/_Prefabs/map building/wrapping bounding box.prefab", typeof(GameObject)) as GameObject;
+    }
+
+    //Create window
+    [MenuItem("Window/Game Options")]
+    static void ShowWindow()
+    {
+        EditorWindow.GetWindow<GameOptions>().Show();
+    }
+
+    private void OnGUI()
+    {
+        //Display button
+        wrapping = EditorGUILayout.Toggle("Player wrap", wrapping);
+        //Set prefab active/deactive
+        wrappingPrefab.SetActive(wrapping);
+       
+    }
+}
