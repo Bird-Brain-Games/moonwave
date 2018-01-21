84d9241414b2ff182dcba3039f06b724c427748d
Graham Watson <12705535+Goombario@users.noreply.github.com>
2018-01-20 21:10:40 -0500

    [ADD] Player Scoreboard fills colours from players, displays at end of match

---
+++ Assets/PlayerScoreboardSection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScoreboardSection : MonoBehaviour {
+
+	public Text scoreText;
+	public Image image;
+	public int score;
+}
